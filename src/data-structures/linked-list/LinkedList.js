37287bc0d3fe7763ffa0065aa03fe4758f350218
Hitesh Kumar <hk.skit@gmail.com>
2018-07-05 21:35:41 +0530

    Added CURD for linked list.

--- src/data-structures/linked-list/LinkedList.js
+++ src/data-structures/linked-list/LinkedList.js
@@ -1,4 +1,4 @@
-const LinkedList = require("./LinkedListNode");
+const LinkedListNode = require("./LinkedListNode");
 
 class LinkedList {
   constructor() {
@@ -9,24 +9,91 @@ class LinkedList {
    * Adds a value to the beginning/head of the list.
    * @param {*} value
    */
-  prepend(value) {}
+  prepend(value) {
+    this.head = new LinkedListNode(value, this.head);
+    return this;
+  }
 
   /**
    * Adds a value to the end/tail of the list.
    * @param {*} value
    */
-  append(value) {}
+  append(value) {
+    const node = new LinkedListNode(value);
+    if (!this.head) {
+      this.head = node;
+      return this;
+    }
+    let current = this.head;
+    while (current.next !== null) {
+      current = current.next;
+    }
+    current.next = node;
+    return this;
+  }
 
   /**
    * Remove the item from linked list.
    * @param {*} value
    */
-  remove(value) {}
+  remove(value) {
+    let current = this.head;
+    let prev = null;
+    while (current !== null) {
+      if (current.value === value) {
+        break;
+      }
+      prev = current;
+      current = current.next;
+    }
+    //  if value not found.
+    if (current === null) {
+      return null;
+    }
+    // if value is head.
+    if (prev === null) {
+      return this.removeHead();
+    }
+    prev.next = current.next; // Moving pointer to next node.
+    current.next = null; // Delete pointer from current.
+    return current;
+  }
 
-  removeHead() {}
+  /**
+   * Move head to the next element to the head.
+   */
+  removeHead() {
+    if (!this.head) {
+      return null;
+    }
+    const head = this.head; // head node
+    this.head = this.head.next; // shift head to next node
+    head.next = null; // remove pointer to next
+    return head;
+  }
 
-  removeTail() {}
+  /**
+   * Move tail to the prev element to the tail.
+   */
+  removeTail() {
+    let current = this.head;
+    let prev = null;
+    while (current.next !== null) {
+      prev = current;
+      current = current.next;
+    }
+    // if both head and tail are same.
+    if (prev === null) {
+      return this.removeHead();
+    }
+    prev.next = null; // remove pointer to tail.
+    return current;
+  }
 
+  /**
+   *
+   * @param {*} value
+   */
   find(value) {
     let current = this.head;
     while (current !== null) {
@@ -38,9 +105,24 @@ class LinkedList {
     return null;
   }
 
-  toArray() {}
+  /**
+   * @return {LinkedListNode[]}
+   */
+  toArray() {
+    const array = [];
+    let current = this.head;
+    while (current !== null) {
+      array.push(current);
+      current = current.next;
+    }
+    return array;
+  }
 
-  toString() {}
+  toString(callback) {
+    return this.toArray()
+      .map(node => node.toString(callback))
+      .toString();
+  }
 }
 
 module.exports = LinkedList;
