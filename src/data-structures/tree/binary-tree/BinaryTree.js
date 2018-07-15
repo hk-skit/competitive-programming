445f31589e7bec85ae5329ef80d4d5c2db72e0f8
Hitesh Kumar <hk.skit@gmail.com>
2018-07-15 07:46:46 +0530

    BinaryTree: added Level Order Traversal(BFS).

--- src/data-structures/tree/binary-tree/BinaryTree.js
+++ src/data-structures/tree/binary-tree/BinaryTree.js
@@ -1,7 +1,32 @@
 const BinaryTreeNode = require('./BinaryTreeNode');
+const Queue = require('../../queue/Queue');
 class BinaryTree {
   constructor() {
     this.root = null;
+  }
+
+  /**
+   * Traverse a tree in Level order that is BFS.
+   * @return {Array}
+   */
+  traverseInLevelOrder() {
+    if (this.root === null) {
+      return [];
+    }
+    const array = [];
+    const queue = new Queue();
+    queue.enqueue(this.root);
+    while (!queue.isEmpty()) {
+      const node = queue.dequeue();
+      array.push(node.value);
+      if (node.left !== null) {
+        queue.enqueue(node.left);
+      }
+      if (node.right !== null) {
+        queue.enqueue(node.right);
+      }
+    }
+    return array;
   }
 
   /**
@@ -14,20 +39,20 @@ class BinaryTree {
       this.root = node;
       return this;
     }
-    // TODO: Use Queue class here when it's available.
-    const queue = [this.root];
-    while (queue.length) {
-      const _node = queue.shift();
+    const queue = new Queue();
+    queue.enqueue(this.root);
+    while (!queue.isEmpty()) {
+      const _node = queue.dequeue();
       if (_node.left === null) {
         _node.setLeft(node);
         break;
       }
-      queue.push(_node.left);
+      queue.enqueue(_node.left);
       if (_node.right === null) {
         _node.setRight(node);
         break;
       }
-      queue.push(_node.right);
+      queue.enqueue(_node.right);
     }
     return this;
   }
--- src/data-structures/tree/binary-tree/BinaryTree.spec.js
+++ src/data-structures/tree/binary-tree/BinaryTree.spec.js
@@ -10,6 +10,66 @@ describe('BinaryTree', () => {
     expect(tree.root).to.equal(null);
   });
 
+  describe('BinaryTree.traverseInLeverOrder', () => {
+    it('should return an empty array cause tree is empty', () => {
+      const tree = new BinaryTree();
+      expect(tree.traverseInLevelOrder()).to.deep.equal([]);
+    });
+
+    it('should return an array with single element cause tree has only root', () => {
+      const tree = new BinaryTree();
+      tree.root = new BinaryTreeNode(10);
+      expect(tree.traverseInLevelOrder()).to.deep.equal([10]);
+    });
+
+    it('should return an array with for tree with has only root and left child', () => {
+      const tree = new BinaryTree();
+      tree.root = new BinaryTreeNode(10);
+      tree.root.left = new BinaryTreeNode(20);
+      expect(tree.traverseInLevelOrder()).to.deep.equal([10, 20]);
+    });
+
+    it('should return an array with for tree with has only root and right child', () => {
+      const tree = new BinaryTree();
+      tree.root = new BinaryTreeNode(10);
+      tree.root.right = new BinaryTreeNode(20);
+      expect(tree.traverseInLevelOrder()).to.deep.equal([10, 20]);
+    });
+
+    it('should return an array with for filled tree', () => {
+      const tree = new BinaryTree();
+      tree.insertInLevelOrder(10);
+      tree.insertInLevelOrder(20);
+      tree.insertInLevelOrder(30);
+      /**
+       *     10
+       *    /   \
+       *  20     30
+       *
+       */
+      expect(tree.traverseInLevelOrder()).to.deep.equal([10, 20, 30]);
+    });
+
+    it('should return an array with for tree', () => {
+      const tree = new BinaryTree();
+      /**
+       * Before insertion.
+       *            10
+       *           /   \
+       *         11     9
+       *        /      /  \
+       *      7       15    8
+       */
+      tree.root = new BinaryTreeNode(10);
+      tree.root.setLeft(new BinaryTreeNode(11));
+      tree.root.left.setLeft(new BinaryTreeNode(7));
+      tree.root.setRight(new BinaryTreeNode(9));
+      tree.root.right.setLeft(new BinaryTreeNode(15));
+      tree.root.right.setRight(new BinaryTreeNode(8));
+      expect(tree.traverseInLevelOrder()).to.deep.equal([10, 11, 9, 7, 15, 8]);
+    });
+  });
+
   describe('BinaryTree.insertInLevelOrder', () => {
     it('should insert node in empty tree', () => {
       const tree = new BinaryTree();
